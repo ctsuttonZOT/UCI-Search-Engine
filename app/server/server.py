090bf66021eaf8fff1cb6159bdf60f10f4c2308c
Anastacio Ruiz <anastar1@uci.edu>
2025-05-28 23:02:55 -0700

    updated server to now work with json

--- app/server/server.py
+++ app/server/server.py
@@ -1,12 +1,16 @@
 from flask import Flask
 from flask import request
-
+from flask_cors import CORS
 app = Flask(__name__)
 
 # curl --header "Content-Type: application/json" --request POST --data '{"username":["xyz"]}' http://127.0.0.1:5000
-
+# python -m flask --app server run
+CORS(app)
 @app.route("/search", methods=['POST'])
 def hell_world():
+    # parse the query
+    # call the query search function
+    # return results
 
     print(request.get_json(request.get_data())['username'])
 
